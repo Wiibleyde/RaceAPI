bd62b10606557340202d8fcb7714672b8b1aca91
HugoANDRIAMAMPIANINA <hugo.andria15@gmail.com>
2024-12-05 14:32:26 +0100

    add default route /

--- src/web.py
+++ src/web.py
@@ -4,6 +4,10 @@ app = FastAPI()
 
 DEFAULT_PATH = "/api/v1"
 
+@app.get("/", description="default route", include_in_schema=False, status_code=200)
+async def get_default_route():
+    return "OK"
+
 from src.api.stables import *
 from src.api.pilots import *
 from src.api.races import *
