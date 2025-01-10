f099b59a1bce6186b84071cd62a7717140096ca0
HugoANDRIAMAMPIANINA <hugo.andria15@gmail.com>
2025-01-10 12:11:56 +0100

    merged auth into dev, added auth test in tests.py, added auth headers to unit tests, removed debug info

--- src/web.py
+++ src/web.py
@@ -7,19 +7,15 @@ import os
 app = FastAPI()
 
 DEFAULT_PATH = "/api/v1"
-# dotenv_file_path = "./.env"
 dotenv_file_path = os.path.join(os.path.dirname(__file__), "..", ".env")
-print(dotenv_file_path)
 
 if not os.path.exists(dotenv_file_path):
     print(".env file not found, please refer to the README.md")
     exit(1)
 
 load_dotenv(dotenv_path=dotenv_file_path)
-# load_dotenv(find_dotenv())
 
 API_KEY = os.getenv("API_KEY")
-print(API_KEY)
 if API_KEY is None:
     print("API key not found, please refer to the README.md to setup one")
     exit(1)
